567cb999ba2ba7136a1e0dc1ca9c6ba291a88cdb
Alexandre Conrado <alexandre.conrado@vnda.com.br>
2019-02-05 09:20:41 -0200

    Apply python code style

--- classes.py
+++ classes.py
@@ -1,135 +1,136 @@
 import os
 import re
 
+
 class FileImporter:
-  BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+    BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
-  IN_PATH = 'in/'
-  ROW_SEPARATOR = '\n';
-  COL_SEPARATOR = ','
+    IN_PATH = 'in/'
+    ROW_SEPARATOR = '\n'
+    COL_SEPARATOR = ','
 
-  
-  input_filename = None
-  file_lines = []
-  salesmans = []
-  customers = []
-  sales = []
-  top_sale = {"sale_id": None, "value": 0}
-  worst_salesman = None
+    input_filename = None
+    file_lines = []
+    salesmans = []
+    customers = []
+    sales = []
+    top_sale = {"sale_id": None, "value": 0}
+    worst_salesman = None
 
-  def input_file(self, filename):
-    self.input_filename = filename
+    def input_file(self, filename):
+        self.input_filename = filename
 
-  def output_path(self, pathname):
-    self.output_path = pathname    
+    def output_path(self, pathname):
+        self.output_path = pathname
 
-  # Loads info from file
-  def load(self):
+    # Loads info from file
+    def load(self):
 
-    filepath = '{}/{}{}'.format(
-      self.BASE_PATH,
-      self.IN_PATH,
-      self.input_filename
-    )
-    with open(filepath) as file:
-      lines = file.readlines()
-    if lines:
-      self.file_lines = lines
+        filepath = '{}/{}{}'.format(
+            self.BASE_PATH,
+            self.IN_PATH,
+            self.input_filename
+        )
+        with open(filepath) as file:
+            lines = file.readlines()
+        if lines:
+            self.file_lines = lines
 
-  # Parse a single line
-  def parse_line(self, line):
-    # Treats the sale list
-    if line.find('[') > 0:
-      str1 = line.split('[', 1)[1].split(']')[0]
-      str2 = str1.replace(',', '|')
-      line = line.replace(str1, str2)
-      line = line.replace('[', '').replace(']', '')
-    
-    return line.split(self.COL_SEPARATOR)
+    # Parse a single line
+    def parse_line(self, line):
+        # Treats the sale list
+        if line.find('[') > 0:
+            str1 = line.split('[', 1)[1].split(']')[0]
+            str2 = str1.replace(',', '|')
+            line = line.replace(str1, str2)
+            line = line.replace('[', '').replace(']', '')
 
-  def list_items(self, sale_id, sale_data):
-    sale_list = []
-    sales = sale_data.split('|')
-    
-    for sale in sales:
-      detail = sale.split('-')
-      quantity = int(detail[1])
-      price  = float(detail[2])
+        return line.split(self.COL_SEPARATOR)
 
-      if price * quantity > self.top_sale['value']:
-        self.top_sale['sale_id'] = sale_id
-        self.top_sale['value'] = price * quantity
+    def list_items(self, sale_id, sale_data):
+        sale_list = []
+        sales = sale_data.split('|')
 
-      detail = {
-        "id": detail[0], 
-        "quantity": quantity,
-        "price": price
-      }
-      sale_list.append(detail)
+        for sale in sales:
+            detail = sale.split('-')
+            quantity = int(detail[1])
+            price = float(detail[2])
 
-    return sale_list
+            if price * quantity > self.top_sale['value']:
+                self.top_sale['sale_id'] = sale_id
+                self.top_sale['value'] = price * quantity
 
+            detail = {
+                "id": detail[0],
+                "quantity": quantity,
+                "price": price
+            }
+            sale_list.append(detail)
 
-  # maps the columns meanin
-  def map(self, line_fields):
-    if line_fields[0] == '001':
-      salary = float(line_fields[3].replace('\n', ''))
+        return sale_list
 
-      data = {
-        "type": "salesman", 
-        "cpf": line_fields[1],
-        "name": line_fields[2],
-        "salary": salary
-      }
-      
-      if not self.worst_salesman:
-        self.worst_salesman = data
-      elif data['salary'] < self.worst_salesman['salary']:
-        self.worst_salesman = data
+    # maps the columns meanin
+    def map(self, line_fields):
+        if line_fields[0] == '001':
+            salary = float(line_fields[3].replace('\n', ''))
 
-      self.salesmans.append(data)
-    elif line_fields[0] == '002':
-      data = {
-        "type": "customer", 
-        "cnpj": line_fields[1],
-        "name": line_fields[2],
-        "area": line_fields[3]
-      }
-      self.customers.append(data)
-    elif line_fields[0] == '003':
-      data = {
-        "type": "sales", 
-        "sale_id": line_fields[1],
-        "sales": self.list_items(line_fields[1], line_fields[2]),
-        "salesman": line_fields[3]
-      }
-      self.sales.append(data)
+            data = {
+                "type": "salesman",
+                "cpf": line_fields[1],
+                "name": line_fields[2],
+                "salary": salary
+            }
 
-  # writes info to output file
-  def write(self):
-    filepath = '{}/{}{}'.format(
-      self.BASE_PATH,
-      self.output_path,
-      "{}.done.dat".format(self.input_filename)
-    )    
-    
-    file = open(filepath,'w+')
-    file.truncate(0) 
-    file.write("* Amount of clients: {}\n".format(len(self.salesmans)))
-    file.write("* Amount of salesman: {}\n".format(len(self.customers)))
-    file.write("* Most expensive sale (ID): {}\n".format(self.top_sale['sale_id']))
-    file.write("* Worst salesman ever: {}\n".format(self.worst_salesman['name']))
-    file.close()
+            if not self.worst_salesman:
+                self.worst_salesman = data
+            elif data['salary'] < self.worst_salesman['salary']:
+                self.worst_salesman = data
 
-  def run(self):
-    if not self.input_filename:
-      return False
+            self.salesmans.append(data)
+        elif line_fields[0] == '002':
+            data = {
+                "type": "customer",
+                "cnpj": line_fields[1],
+                "name": line_fields[2],
+                "area": line_fields[3]
+            }
+            self.customers.append(data)
+        elif line_fields[0] == '003':
+            data = {
+                "type": "sales",
+                "sale_id": line_fields[1],
+                "sales": self.list_items(line_fields[1], line_fields[2]),
+                "salesman": line_fields[3]
+            }
+            self.sales.append(data)
 
-    self.load()
-    
-    for line in self.file_lines:
-      line_fields = self.parse_line(line)
-      self.map(line_fields)
+    # writes info to output file
+    def write(self):
+        filepath = '{}/{}{}'.format(
+            self.BASE_PATH,
+            self.output_path,
+            "{}.done.dat".format(self.input_filename)
+        )
 
-    self.write()
+        file = open(filepath, 'w+')
+        file.truncate(0)
+        file.write("* Amount of clients: {}\n".format(len(self.salesmans)))
+        file.write("* Amount of salesman: {}\n".format(len(self.customers)))
+        file.write(
+            "* Most expensive sale (ID): {}\n".format(self.top_sale['sale_id'])
+        )
+        file.write(
+            "* Worst salesman ever: {}\n".format(self.worst_salesman['name']))
+        file.close()
 
+    def run(self):
+        if not self.input_filename:
+            return False
+
+        self.load()
+
+        for line in self.file_lines:
+            line_fields = self.parse_line(line)
+            self.map(line_fields)
+
+        self.write()
--- file_importer.py
+++ file_importer.py
@@ -3,10 +3,10 @@ import glob
 from classes import FileImporter
 
 importer = FileImporter()
-#importer.output_parh('%HOMEPATH%/data/out/')
+# importer.output_parh('%HOMEPATH%/data/out/')
 importer.output_path('out/')
 
 for f in glob.glob('in/*'):
-	path = f.split('/')
-	importer.input_file(path[1])
-	importer.run()
+    path = f.split('/')
+    importer.input_file(path[1])
+    importer.run()
